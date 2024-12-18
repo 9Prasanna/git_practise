1110939c612cda4bb33e94c4f7f106626b80b21e
9Prasanna <kasireddyprasannakumarreddy@gmail.com>
2024-12-18 21:08:11 +0530

    Update first_gitpractise.py

--- first_gitpractise.py
+++ first_gitpractise.py
@@ -1,13 +1,11 @@
+
+# code modification has been done 
+# changes that are made are removing the flag and making it as simple by providing the return statement directly
 def isprime(number):
-  flag = 0
   for i in range(2,number):
     if number%2==0:
-     flag = 1
+      return 'not prime'
     else:
-      flag = 0
-  if flag == 0:
-    return 'prime'
-  else:
-    return 'not prime'
+      return 'prime'
 
 print(isprime(3))
